710f2d6fe76678b0f1c184cb44be539d457124e4
Thiago Tavares <thiago.t@tamanna.com>
2022-09-13 23:21:59 +0100

    add icon in publish button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
 import logoImg from './assets/logo-nlw-esports.svg';
 import './styles/main.css';
 function App() {
@@ -72,7 +73,8 @@ function App() {
             </span>
           </div>
 
-          <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-700">
+          <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
             Publicar Anúncio
           </button>
         </div>
